5afb1f69d603132004e227bda961f7e8f88003a7
sanoj166 <j_haggu@hotmail.com>
2023-09-11 09:59:17 +0200

    Add project files.

---
+++ lektion 09-11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lektion_09_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jonas");
+        }
+    }
+}
