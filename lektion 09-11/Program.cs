943d99cb48a1dac68dd678e0a3d9f722cc83fc9b
sanoj166 <j_haggu@hotmail.com>
2023-09-11 11:02:54 +0200

    added vending machine

--- lektion 09-11/Program.cs
+++ lektion 09-11/Program.cs
@@ -1,12 +1,72 @@
 ﻿namespace lektion_09_11
 {
+
+    enum Beverage
+    {
+        Coffee,
+        Tea,
+        Water,
+        Soda,
+        Juice
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Jonas");
-            Console.WriteLine("Dettta är version2");
-            Console.WriteLine("123123");
+
+            Console.WriteLine("Welcome to the awesome vending machine");
+            Console.WriteLine("Which beverage do you prefer? Coffee, Tea, Water, Soda, Juice");
+            String userInput = Console.ReadLine();
+
+            switch (userInput.ToLower()) 
+            
+            {
+
+                case "coffee":
+                    Respond(Beverage.Coffee);
+                    break;
+                case "tea":
+                    Respond(Beverage.Tea); 
+                    break;
+                case "water":
+                    Respond(Beverage.Water);
+                    break;
+                case "soda":
+                    Respond(Beverage.Soda);
+                    break;
+                case "juice":
+                    Respond(Beverage.Juice);
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, this drink does not exist");
+                    break;
+
+
+            
+            }
+
+        }
+        static void Respond(Beverage drink)
+        {
+            switch (drink)
+            {
+                case Beverage.Coffee:
+                    Console.WriteLine("You ordered Kaffe latte");
+                    break;
+                case Beverage.Tea:
+                    Console.WriteLine("You ordered Tea, yucky");
+                    break;
+                case Beverage.Water:
+                    Console.WriteLine("You choose water, why though?");
+                    break;
+                case Beverage.Soda:
+                    Console.WriteLine("Nice choice, pepsi cola");
+                    break;
+                case Beverage.Juice:
+                    Console.WriteLine("one orange juice coming your way!");
+                    break;
+            }
         }
     }
 }
